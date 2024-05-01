18be5e7777c30305f515832a9fe0e1ab253fbc75
Lilibeth Neal <mejialily036@gmail.com>
2024-05-01 16:22:20 -0500

    added some styling

--- public/index.js
+++ public/index.js
@@ -1,23 +1,20 @@
 const accordEl= document.getElementById('accordionExample');
 
+const price= ['20,000', '50,000', '30,000', '80,000', '60,000', '75,000'];
+const carMpg= [27, 33, 40, 25];
+const carSeats= [4, 5, 7];
 
+  const getCars = async () => {
+    const result = await fetch('https://exam.razoyo.com/api/cars', {
+      method: 'GET',
+    });
+    const json = await result.json();
+    return json;
+  };
 
-fetch('https://exam.razoyo.com/api/cars', {
-  method: 'GET', //GET is the default.
-  credentials: 'same-origin', // include, *same-origin, omit
-  redirect: 'follow', // manual, *follow, error
-})
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    console.log(data.cars[0]);
-    console.log(data.cars[1]);
-    console.log(data.makes[0]);
-  });
 
   const renderCars= (data) => {
+    console.log(data);
     const accordItem= document.createElement('div');
     const accordHeader= document.createElement('h2');
     const accordButton= document.createElement('button');
@@ -30,10 +27,22 @@ fetch('https://exam.razoyo.com/api/cars', {
     accordHeader.classList.add('accordion-header');
     accordButton.classList.add('accordion-button');
 
+    accordButton.setAttribute('type', 'button');
+    accordButton.setAttribute('data-bs-toggle', 'collapse');
+    accordButton.setAttribute('data-bs-target', '#collapseOne');
+    accordButton.setAttribute('aria-expanded', 'true');
+    accordButton.setAttribute('aria-controls', 'collapseOne');
+
     accordCollapse.classList.add('accordion-collapse', 'collapse', 'show');
+    accordCollapse.setAttribute('id', 'collapseOne');
+    accordCollapse.setAttribute('data-bs-parent', '#accordionExample');
     accordBody.classList.add('accordion-body');
 
-    accordButton.innerHTML= `hello`;
+   
+
+    accordButton.innerHTML= `${data.cars[0].year} ${data.cars[0].make} ${data.cars[0].model}`;
+    accordBody.innerHTML= `Price: ${price[0]} MPG: ${carMpg[0]} Seats: ${carSeats[0]}`
+
 
     
     accordEl.appendChild(accordItem);
@@ -48,33 +57,7 @@ fetch('https://exam.razoyo.com/api/cars', {
 
   }
 
-  renderCars();
 
 
 
-
-// const requestUrl= 'https://exam.razoyo.com/api/cars';
-
-// function getApi(requestUrl) {
-//     fetch(requestUrl)
-//       .then(function (response) {
-//         console.log(response);
-//         if (response.status === 200) {
-//           console.log(response.status);
-//         }
-//         return response.json();
-//     });
-//   }
-  
-//   getApi(requestUrl);
-
-
-// const data = async () => {
-//     const result = await fetch('https://exam.razoyo.com/api/cars', {
-//       method: 'GET',
-//     });
-//     const json = await result.json();
-//     console.log(json);
-//     // return json;
-//   };
-
+  getCars().then((response) => renderCars(response));
