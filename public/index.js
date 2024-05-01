846d283642ecfa1037b910cb0a0c4d033e80983b
Lilibeth Neal <mejialily036@gmail.com>
2024-05-01 12:50:03 -0500

    added to  index.js

---
+++ public/index.js
@@ -0,0 +1,17 @@
+
+fetch('https://exam.razoyo.com/api/cars', {
+  method: 'GET', //GET is the default.
+  credentials: 'same-origin', // include, *same-origin, omit
+  redirect: 'follow', // manual, *follow, error
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const path= require('path');
+
+
 // Initialize an instance of Express.js
 const app = express();
-
 const PORT= 3001;
 
 // Static middleware pointing to public folder 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname,'index.html'));
 })
+
+
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`);
